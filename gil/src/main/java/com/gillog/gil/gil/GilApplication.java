9b13a57df2fb0346bf6b54efa18837eec790de5f
swgil007 <swgil007@naver.com>
2022-01-07 08:04:34 +0900

    init

---
+++ gil/src/main/java/com/gillog/gil/gil/GilApplication.java
@@ -0,0 +1,13 @@
+package com.gillog.gil.gil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GilApplication.class, args);
+	}
+
+}
---
+++ gil/src/test/java/com/gillog/gil/gil/GilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gillog.gil.gil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
